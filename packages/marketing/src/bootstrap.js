b81b8cf83eb1ddc2134409a858988af3dfb24aa9
adrien <adrien.kowalczyk>
2022-09-20 14:51:13 +0200

    push from udemy folders

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -2,27 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// mount function to start up the app
+// Mount function to start up the app
 const mount = (el) => {
-    ReactDOM.render(
-        <App />,
-        el
-    )
-}
+  ReactDOM.render(<App />, el);
+};
 
-
-// if we are in developemnt and in isolation 
+// If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
-    if (devRoot) {
-        mount(devRoot)
-    }
+  const devRoot = document.querySelector('#_marketing-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
 }
 
-
-// we are running through container 
+// We are running through container
 // and we should export the mount function
-
-
-export {mount}
+export { mount };
