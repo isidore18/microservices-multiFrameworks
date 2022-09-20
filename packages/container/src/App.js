73506c04bc7d72d945c7fdec9d7524cb69fc2e3d
adrien <adrien.kowalczyk>
2022-09-20 16:38:05 +0200

    test folders form udemy

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default function App () {
     return (
         <div>
-            <h1>Hi there !!!! anmojn! !w!hat</h1>
+            <h1>Hi there !!!!  ssfanmojn! !w!hat</h1>
             <hr />
             <MarketingApp />
         </div>
