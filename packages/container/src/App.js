d44f37d6ff4823badd2000e07d533b13664eb932
adrien <adrien.kowalczyk>
2022-09-21 10:14:53 +0200

    typo in command fix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default function App () {
     return (
         <div>
-            <h1>fi x </h1>
+            <h1>fi  </h1>
             <hr />
             <MarketingApp />
         </div>
