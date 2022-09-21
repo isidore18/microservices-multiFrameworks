4ff118eaa0bbaf38bb2a256d08e5a86baaadfe17
adrien <adrien.kowalczyk>
2022-09-21 10:20:15 +0200

    removed invalidation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default function App () {
     return (
         <div>
-            <h1>fi  </h1>
+            <h1>fi  dfs </h1>
             <hr />
             <MarketingApp />
         </div>
