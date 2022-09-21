966aadbbde23bc000d6a4a966a13da2f671aa17b
adrien <adrien.kowalczyk>
2022-09-21 09:53:03 +0200

    test wiht shyniinc + aws region

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default function App () {
     return (
         <div>
-            <h1>Hi there !ddsgsgqdfghsdhsdgfsgfgqdqgq dsgdgh!!!  ssfanmoxvwjn! !w!hat</h1>
+            <h1>Hi there !dfgqdqgq dsgdgh!!!  ssfanmoxvwjn! !w!hat</h1>
             <hr />
             <MarketingApp />
         </div>
