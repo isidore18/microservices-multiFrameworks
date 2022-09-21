8afd4fee7d3a997262b499683a2621c033e843c2
adrien <adrien.kowalczyk>
2022-09-21 10:09:56 +0200

    added invalidation section

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default function App () {
     return (
         <div>
-            <h1>sigh  sf3</h1>
+            <h1>fix</h1>
             <hr />
             <MarketingApp />
         </div>
