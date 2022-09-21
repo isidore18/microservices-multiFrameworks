738336b1a6adf3ea7f1b09f8748bc59397cadd1d
adrien <adrien.kowalczyk>
2022-09-21 09:56:57 +0200

    sigh

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default function App () {
     return (
         <div>
-            <h1>Hi there !dfgqdqgddgq dsgdgh!!!  ssfanmoxvwjn! !w!hat</h1>
+            <h1>Hi there !dfgqdqgddgdsffq dsgdgh!!!  ssfanmoxvwjn! !w!hat</h1>
             <hr />
             <MarketingApp />
         </div>
