c72e7e541e5f9aa056abf2d4f788fe70c86ad2f1
adrien <adrien.kowalczyk>
2022-09-21 10:02:00 +0200

    back to udemy folders + shi

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default function App () {
     return (
         <div>
-            <h1>Hi there !dfgqdqgddgdsffq dsgdgh!!!  ssfanmoxvwjn! !w!hat</h1>
+            <h1>sigh</h1>
             <hr />
             <MarketingApp />
         </div>
