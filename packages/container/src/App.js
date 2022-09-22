d0c8a9fe7f942e408847647c52c8582a577fe51e
adrien <adrien.kowalczyk>
2022-09-22 10:14:05 +0200

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -8,6 +8,7 @@ export default function App() {
         <BrowserRouter>
             <div>
                 <Header />
+                Test
                 <hr />
                 <MarketingApp />
             </div>
